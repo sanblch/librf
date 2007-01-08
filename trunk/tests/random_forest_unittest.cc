0393724d989d4e981d47891a3e1f340c52d70771
benjamin.n.lee <benjamin.n.lee@8bacb8f9-7126-0410-8249-17c245e73d71>
2007-01-08 06:43:23 +0000

    tested CSV file loading

--- trunk/tests/random_forest_unittest.cc
+++ trunk/tests/random_forest_unittest.cc
@@ -10,7 +10,9 @@ using namespace librf;
 struct RF_TrainPredictFixture {
   RF_TrainPredictFixture() {
     cout << "loading heart data" << endl;
-    heart_ = InstanceSet::load_from_libsvm("../data/heart.svm", 14);
+//    heart_ = InstanceSet::load_libsvm("../data/heart.svm", 14);
+    heart_ = InstanceSet::load_csv_and_labels("../data/heart.csv",
+                                           "../data/heart_labels.txt",true);
   }
   ~RF_TrainPredictFixture() {
     delete heart_;
@@ -20,7 +22,7 @@ struct RF_TrainPredictFixture {
 
 TEST_FIXTURE(RF_TrainPredictFixture, TrainPredictCheck) {
 
-  RandomForest rf(*heart_, 10, 12, 12);
+  RandomForest rf(*heart_, 100, 12, 12);
   // rf.print();
   cout << "Training accuracy " << rf.training_accuracy() <<endl;
   cout << "OOB Accuracy " << rf.oob_accuracy() <<endl;
@@ -35,7 +37,7 @@ TEST_FIXTURE(RF_TrainPredictFixture, TrainPredictCheck) {
   vector< pair<float, int> > scores;
   rf.variable_importance(&scores, &seed);
   for (int i = 0; i < scores.size(); ++i) {
-    cout << scores[i].second << ":" << scores[i].first <<endl;
+    cout << heart_->get_varname(scores[i].second) << ":" << scores[i].first <<endl;
   }
 }
 /*
