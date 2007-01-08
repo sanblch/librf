ca441a4c56bbe29edd5f8e8a03f260aa751e39c6
benjamin.n.lee <benjamin.n.lee@8bacb8f9-7126-0410-8249-17c245e73d71>
2007-01-08 05:22:36 +0000

    Changed interface to instance_set (named constructors idiom only)

--- librf/instance_set.cc
+++ librf/instance_set.cc
@@ -1,21 +1,130 @@
-/*****************************
-Ben Lee (benlee@ece.ucsb.edu)
-
-Instance Set
-
-*****************************/
 #include "librf/instance_set.h"
 #include <fstream>
+#include <string>
+#include <iostream>
 #include <float.h>
 #include "librf/weights.h"
 #include "librf/types.h"
+#include "librf/stringutils.h"
+
+using namespace std;
 
 namespace librf {
-
-const int InstanceSet::kLeft = 0;
-const int InstanceSet::kRight = 1;
 InstanceSet::InstanceSet(){}
+/***
+ * Named constructor for loading from a csv file and a label file
+ *
+ * @param csv_data CSV filename
+ * @param header whether there is a header with var. names
+ * @param delim CSV delimiter - defaults to ','
+ */
+InstanceSet* InstanceSet::load_from_csv_and_labels(const string& csv_data,
+                                      const string& label_file,
+                                      bool header,
+                                      const string& delim) {
+  return new InstanceSet(csv_data, label_file, header, delim);
+}
 
+/**
+ * Named constructor for creating a subset from an existing set
+ * 
+ * @param set existing set 
+ * @param wl non-zeroes weights
+ */
+InstanceSet* InstanceSet::create_subset(const InstanceSet& set,
+                                     const weight_list& wl) {
+  return new InstanceSet(set, wl);
+}
+
+/**
+ * Named constructor for loading from a .libsvm format
+ *
+ */
+InstanceSet* InstanceSet::load_from_libsvm(const string& data, int nf) {
+  return new InstanceSet(data, nf);
+}
+
+/***
+ * Unnamed private constructor for loading csv file/label file
+ */
+InstanceSet::InstanceSet(const string& csv_data,
+            const string& label_file,
+            bool header, const string& delim) {
+  ifstream data(csv_data.c_str());
+  load_csv(data, header, delim);
+
+  ifstream labels(label_file.c_str());
+  load_labels(labels);
+}
+
+
+
+/***
+ * Load labels from an istream
+ *
+ * LIMITATION: assumes binary labels... this needs to change
+ */
+void InstanceSet::load_labels(istream&in) {
+  float label;
+  int true_label;
+  while (in >> label) {
+    if (label == -1.0) {
+      true_label =0;
+    } else if (label ==0.0) {
+      true_label =0;
+    } else if (label ==1.0) {
+      true_label =1;
+    } else {
+      cerr << "Incorrect label (only +1, 0, -1 supported)" << endl;
+      assert(false);
+    }
+    labels_.push_back(true_label);
+    distribution_.add(true_label);
+  }
+}
+/***
+ * Load csv from an istream
+ */
+void InstanceSet::load_csv(istream&in, bool header, const string& delim) {
+  // read variable names
+  int num_features = -1;
+  string buffer;
+  getline(in, buffer);
+  if (header) {
+    StringUtils::split(buffer, &var_names_);
+    num_features = var_names_.size();
+  } else {
+    vector<string> ary;
+    StringUtils::split(buffer, &ary);
+    num_features = ary.size();
+    // reset istream
+    in.seekg (0, ios::beg);
+    // create dummy var names
+    create_dummy_var_names(num_features);
+    attributes_.resize(num_features);
+  }
+  // get all data
+  while(getline(in, buffer)) {
+    vector<string> ary;
+    StringUtils::split(buffer, &ary);
+    assert(ary.size() == num_features);
+    for (int i = 0; i < ary.size(); ++i) {
+      stringstream ss(ary[i]);
+      //convert to float
+      float val;
+      ss >> val;
+      attributes_[i].push_back(val);
+    }
+  }
+}
+
+void InstanceSet::create_dummy_var_names(int n) {
+  for (int i = 0; i <  n; ++i) {
+    stringstream ss;
+    ss << i;
+    var_names_.push_back(ss.str());
+  }
+}
 
 InstanceSet::InstanceSet(const string& filename, int num_features) : attributes_(num_features) {
     // default to libsvm reading now
@@ -34,22 +143,7 @@ InstanceSet::InstanceSet(const string& filename, int num_features) : attributes_
     distribution_.print();
    cout << "Sorting indices " << endl;
     create_sorted_indices();
-    create_rank_array();
     cout << "Instance Set loaded. " << endl;
-}
-
-void InstanceSet::create_bootstrap(vector<float> * bs) {
-    // randomly sample WITH replacement for the length of the set
-    // returns a float vector of WEIGHTS (default weight is zero);
-    // Allocate and zero weight vector
-    for (int i = 0; i < instances_.size(); ++i) {
-      bs->push_back(0.0);
-    }
-    // Random sampling
-    for (int i = 0; i < instances_.size(); ++i) {
-        int selected = rand() % instances_.size();
-       (*bs)[selected] += 1.0;
-    }
 }
 
 void InstanceSet::create_sorted_indices() {
@@ -61,30 +155,6 @@ void InstanceSet::create_sorted_indices() {
     }
 }
 
-void InstanceSet::create_rank_array() {
-  //allocate properly sized ranks
-  ranks_ = new uint16*[attributes_.size()];
-  for (int i = 0; i < attributes_.size(); ++i) {
-    ranks_[i] = new uint16[attributes_[i].size()];
-  }
-  // for each attribute
-  // get the sorted index order
-  for (int i = 0; i < attributes_.size(); ++i) {
-    const vector<int>& sorted_order = sorted_indices_[i];
-    float last_value = attributes_[i][sorted_order[0]];
-    uint16 rank = 0;
-    ranks_[i][sorted_order[0]] = rank;
-
-    for (int j = 1; j < sorted_order.size(); ++j) {
-      float current_value = attributes_[i][sorted_order[j]];
-      if (current_value > last_value) {
-        rank++;
-        last_value = current_value;
-      }
-      ranks_[i][sorted_order[j]] = rank;
-  }
-  }
-}
 void InstanceSet::sort_attribute(const vector<float>& attribute,
                                  vector<int>*indices) {
     vector<pair<float, int> > pairs;
@@ -96,128 +166,6 @@ void InstanceSet::sort_attribute(const vector<float>& attribute,
         indices->push_back(pairs[i].second);
     }
 }
-// Using the weights as a mask on the instance set -
-// find the best split with respect to this attribute
-void InstanceSet::find_best_split_for_attr(int attr,
-		// const vector<float>& weights,
-    const weight_list& weights,
-    float prior_entropy,
-		float *split_point, float *split_gain) const {
-		// Class distribution of the split 
-		DiscreteDist split_dist[2];
-    const int kLeft = 0;
-    const int kRight = 1;
-		// Move all the instances into RIGHT split at first
-		for (int i = 0; i < weights.size(); ++i) {
-			split_dist[kRight].add(labels_[i],weights[i]);
-		}
-		float best_gain = - DBL_MAX;
-		// float prior_entropy = split_dist[0].entropy_over_classes();
-    // Use the sorted indices
-    const vector<int>& indices = get_sorted_indices(attr);
-		int split_index = 0;
-
-    // More intuitive, but naive version
-    // Get all the splits possible -- mid-points between DISTINCT values
-    vector <float> possible_splits;
-    float last_value = attributes_[attr][indices[0]];
-    // cout << "possible splits on " << attr <<endl;
-    for (int i = 1; i <indices.size(); ++i) {
-      int j = indices[i];
-      float value = attributes_[attr][j];
-      if (value != last_value) {
-        float s = (value + last_value)/2.0;
-        // cout << s << endl;
-        possible_splits.push_back(s);
-        last_value = value;
-      }
-    }
-    //  Out of possible splits, find the best one
-    //  MOVE EVERYTHING THAT IS <= THAN SPLIT POINT INTO LEFT SET
-    int l = 0;
-    for (int k = 0; k < possible_splits.size(); ++k) {
-      float split = possible_splits[k];
-      for (; l < indices.size() &&
-             attributes_[attr][indices[l]] < split; ++l) {
-        int inst = indices[l];
-        split_dist[kRight].remove(labels_[inst], weights[inst]);
-				split_dist[kLeft].add(labels_[inst], weights[inst]);
-      }
-      float split_entropy = DiscreteDist::entropy_conditioned(split_dist, 2);
-      float curr_gain = prior_entropy - split_entropy;
-      if (curr_gain > best_gain) {
-          best_gain = curr_gain;
-          *split_point = split;
-      }
-    }
-		*split_gain = best_gain;
-}
-
-
-// Find the best split from list of attributes
-void InstanceSet::find_best_split(const vector<int>& attrs,
-                //                  const vector<float>& weights,
-                                 const weight_list& weights,
-																 float prior_entropy,
-								int* split_attr, float *split_point, float* split_gain) const{
-	float best_gain = -DBL_MAX;
-	int best_attr = -1;
-	float best_split = -DBL_MAX;
-
-	for (int i = 0; i < attrs.size(); ++i) {
-    int attr =attrs[i];
-		float curr_split;
-		float curr_gain;
-		find_best_split_for_attr(attr, weights, prior_entropy,
-                             &curr_split, &curr_gain);
-    // cout << attr << ":" << curr_split << "->" << curr_gain <<endl;
-		if (curr_gain > best_gain) {
-				best_gain = curr_gain;
-				best_split = curr_split;
-				best_attr = attr;
-		}
-	}
-	*split_attr = best_attr;
-	*split_point = best_split;
-	*split_gain = best_gain;
-}
-
-
-
-void InstanceSet::split_data(const weight_list& parent,
-                        int split_attr, float split_point,
-                        weight_list*& left, weight_list*& right) const{
-   // Calculate new distributions
-   DiscreteDist split_dist[2];
-   for (int i =0; i < parent.size(); ++i) {
-     byte weight = parent[i];
-     if (weight > 0) {
-       float value = attributes_[split_attr][i];
-       if (value < split_point) {
-          split_dist[kLeft].add(labels_[i], weight);
-       } else {
-          split_dist[kRight].add(labels_[i], weight);
-       }
-      }
-   }
-   // Allocate weight lists
-   left = new weight_list(size(), split_dist[kLeft].sum());
-   right = new weight_list(size(), split_dist[kRight].sum());
-   for (int i =0; i < parent.size(); ++i) {
-     byte weight = parent[i];
-     if (weight > 0) {
-       float value = attributes_[split_attr][i];
-       if (value < split_point) {
-          left->add(i, weight);
-       } else {
-          right->add(i, weight);
-       }
-     }
-   }
-   // cout << "left:" << left->sum() <<endl;
-   // cout << "right:" << right->sum() <<endl;
-}
-
 
 // Grab a subset of the instance (for getting OOB data
 // Seems like maybe this should be a static named constructor
--- librf/random_forest.h
+++ librf/random_forest.h
@@ -1,3 +1,8 @@
+/** 
+ * @file 
+ * @brief Randomforest interface
+ * This is the interface to manage a random forest
+ */
 #ifndef _RANDOM_FOREST_H_
 #define _RANDOM_FOREST_H_
 
@@ -10,33 +15,46 @@ namespace librf {
 class Instance;
 class InstanceSet;
 class Tree;
-
+/**
+ * @brief
+ * RandomForest class.  Interface for growing random forests from training
+ * data or loading a random forest from disk.
+ */
 class RandomForest {
   public:
+    /// Empty constructor
     RandomForest();
+    /// Constructor. (Build from training data)
     RandomForest(const InstanceSet& set,
                  int num_trees,
                  int K,
                  int max_depth);
     ~RandomForest();
-        // predict a new instance 
+     /// Method to predict the label
      int predict(const Instance& c) const;
+     /// Method that returns the class probability 
      float predict_prob(const Instance& c) const;
-     // predict an instance from a set        
+     /// Method to predict the label
      int predict(const InstanceSet& set, int instance_no) const;
+     /// Returns test accuracy of a labeled test set
      float testing_accuracy(const InstanceSet& testset) const;
+     /// Returns training accuracy 
      float training_accuracy() const;
+     /// Returns OOB accuracy (unbiased estimate of test accuracy)
      float oob_accuracy() const;
+     /// Variable importance ranking of features
      void variable_importance(vector< pair<float, int> >* ranking,
                               unsigned int* seed) const;
+     /// Load random forest
      void read(istream& i);
+     /// Save random forest
      void write(ostream& o);
      void print() const;
   private:
-    const InstanceSet& set_;
-    vector<Tree*> trees_;
-    int max_depth_;
-    int K_;
+    const InstanceSet& set_; // training data set
+    vector<Tree*> trees_; // component trees in the forest
+    int max_depth_; // maximum depth of trees
+    int K_; // random vars to try per split
 };
 
 } // namespace
